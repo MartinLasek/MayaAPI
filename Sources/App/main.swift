b3a5a0e1f6660c4e93d541dc5238a43641909bf5
Martin Lasek <martinlasek@hotmail.de>
2017-02-11 10:38:55 +0100

    refactoerd respone to be json and image url

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -17,19 +17,16 @@ drop.post("sent-image") { req in
 
   if let contentType = req.headers["Content-Type"], contentType.contains("image/png"), let id = req.headers["id"], let bytes = req.body.bytes {
     
-    // save user
     let user = try userDispatcher.saveUser(phoneUUID: id)
-    
     let image = try imageDispatcher.saveImage(user: user, bytes: bytes)
     let randomImage = try imageDispatcher.getRandomImage()
     
     try imageDispatcher.saveUserSentImage(user: user, image: image)
     try imageDispatcher.saveUserReceivedImage(user: user, image: randomImage)
-    
-    let getURL = URL(fileURLWithPath: randomImage.path).appendingPathComponent(randomImage.name, isDirectory: false)
-    let file = try Data(contentsOf: getURL)
-    
-    return Response(headers: ["Content-Type": "image/png; charset=utf-8"], body: Body(file))
+
+    return try JSON(node: [
+        "image": randomImage.name
+    ])
   }
   
   return "could not save image"
